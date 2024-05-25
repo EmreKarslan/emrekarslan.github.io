dee81daa01ec6fb2437b8bf721b7e09f78e122c0
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-25 23:54:16 +0300

    feat(page): Add projects page

---
+++ src/app/(main)/projects/page.tsx
@@ -0,0 +1,10 @@
+const ProjectsPage = () => {
+  return (
+    <div className="min-h-full flex items-center justify-center flex-col max-w-[60vw] m-auto">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        Projects
+      </div>
+    </div>
+  );
+};
+export default ProjectsPage;
