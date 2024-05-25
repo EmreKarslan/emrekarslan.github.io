8b666f075768a5aeaee57a11755cdbfe0dd818bc
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-25 23:53:55 +0300

    feat(page): Add blog page

---
+++ src/app/(main)/blog/page.tsx
@@ -0,0 +1,10 @@
+const BlogPage = () => {
+  return (
+    <div className="min-h-full flex items-center justify-center flex-col max-w-[60vw] m-auto">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        Blog
+      </div>
+    </div>
+  );
+};
+export default BlogPage;
