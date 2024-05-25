514aa6e88693185cef0d972b4098fbc99fcad6f9
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-25 23:53:22 +0300

    feat(layout): Add logo and navigation buttons to header component

--- src/app/(main)/_components/header.tsx
+++ src/app/(main)/_components/header.tsx
@@ -1,21 +1,62 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
 import { LinkedinButton } from "./linkedin-button";
 import { GitHubButton } from "./github-button";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+import Link from "next/link";
 
 export const Header = () => {
   const { scrolled } = useScrollTop();
+  const currentPath = usePathname();
 
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6 gap-4 max-w-[60vw]",
         scrolled && "border-b shadow-sm"
       )}
     >
+      <Button variant="link" asChild>
+        <Link href="/" passHref>
+          <Logo />
+        </Link>
+      </Button>
+      <Button
+        variant="link"
+        className={currentPath === "/" ? "bg-gray-200 dark:bg-slate-700" : ""}
+        asChild
+      >
+        <Link href="/" passHref>
+          Home
+        </Link>
+      </Button>
+      <Button
+        variant="link"
+        className={
+          currentPath === "/projects" ? "bg-gray-200 dark:bg-slate-700" : ""
+        }
+        asChild
+      >
+        <Link href="/projects" passHref>
+          Projects
+        </Link>
+      </Button>
+      <Button
+        variant="link"
+        className={
+          currentPath === "/blog" ? "bg-gray-200 dark:bg-slate-700" : ""
+        }
+        asChild
+      >
+        <Link href="/blog" passHref>
+          Blog
+        </Link>
+      </Button>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
         <ModeToggle />
         <LinkedinButton />
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "./_components/header";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="h-full flex justify-center">
       <Header />
       <main className="h-full pt-40">{children}</main>
     </div>
