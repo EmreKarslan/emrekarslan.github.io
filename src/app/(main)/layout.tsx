e11a9b407fc9b8d1f0faf5ec23b326ddd93a178c
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-27 22:50:13 +0300

    style(layout): Add width

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./_components/header/header";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex justify-center">
-      <div className="flex h-full 2xl:max-w-[60vw] overflow-hidden relative">
+      <div className="flex h-full w-[100vw] 2xl:w-[60vw]">
         <Header />
         <main className="h-full pt-40 w-full">{children}</main>
       </div>
