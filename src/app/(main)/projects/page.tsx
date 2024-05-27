90c87b464f65df70d874f63fb899e59880e5756e
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-27 22:51:17 +0300

    chore(pages): Add lorem ipsum text

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -2,7 +2,68 @@ const ProjectsPage = () => {
   return (
     <div className="min-h-full flex items-center justify-center flex-col max-w-[60vw] m-auto">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        Projects
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+        cumque enim commodi adipisci id dignissimos ex deserunt doloremque ea
+        voluptates ullam dolores in eaque molestiae incidunt saepe doloribus
+        repellat, dolore architecto sed quibusdam nulla libero mollitia aliquam!
+        Cupiditate ratione consequuntur aspernatur aliquam modi expedita
+        dolorem? Soluta placeat iusto, perspiciatis quibusdam a quaerat ad
+        laboriosam nihil, accusantium, illum incidunt doloribus optio dolorum.
+        Ipsa voluptates assumenda aspernatur rem excepturi sunt temporibus quod
+        repudiandae, aut debitis officiis ipsum commodi animi distinctio in
+        porro earum consequatur blanditiis deserunt quam, ullam inventore
+        repellendus provident? Enim at earum natus, nobis, ipsa maxime aliquid
+        explicabo ab voluptatum non molestiae veritatis esse cum error,
+        doloremque reprehenderit aspernatur perferendis eligendi consequuntur ad
+        repellat dignissimos rerum nisi magni. Esse, possimus? Odit modi
+        recusandae mollitia quibusdam nobis dignissimos hic, ab sunt suscipit
+        consequatur rem ut veniam atque eos soluta impedit amet eaque quas
+        placeat repudiandae. Reiciendis, hic error. Repudiandae, ratione
+        voluptatibus. Nisi nesciunt quis deleniti beatae ducimus maiores
+        assumenda mollitia numquam facilis alias rem, cumque, qui ut vero
+        laboriosam accusamus sunt aliquid in quod. Quis expedita maxime amet,
+        dignissimos autem temporibus nisi ipsa mollitia doloremque cumque
+        obcaecati. Earum ipsum magni dolores in, explicabo at quisquam eius
+        asperiores a excepturi, sunt, atque hic placeat molestias ea fuga nihil.
+        Exercitationem corrupti accusantium in, natus consectetur laborum
+        distinctio maiores, suscipit ipsum eligendi ratione reprehenderit.
+        Facere suscipit eaque dolorem. Tempora, deleniti similique dicta hic
+        animi ut esse dignissimos veniam ex nulla qui iste in explicabo sint
+        inventore, expedita minima impedit alias omnis nam? Perferendis
+        molestias reiciendis omnis iure exercitationem, autem eveniet libero
+        adipisci laboriosam velit reprehenderit quia. Perspiciatis enim
+        repudiandae distinctio facilis voluptate nulla esse, eaque illum,
+        aperiam minus nostrum error eius deleniti reiciendis? Eos ut vero
+        exercitationem praesentium neque mollitia! Ex ab laborum consequatur
+        quia provident molestiae culpa numquam, cupiditate libero dolores,
+        commodi a iusto corrupti excepturi! Eos magni quis blanditiis assumenda
+        est quo nihil incidunt, ipsum aliquam, possimus veniam numquam qui,
+        maxime doloremque dolore illum labore repellat obcaecati quos? Dolorum
+        cumque sed nam culpa quas, nemo iste nesciunt eum totam ab nihil
+        perspiciatis! Quidem, voluptate eaque optio ab explicabo eum vitae rem
+        asperiores incidunt officiis quisquam ratione perspiciatis sunt ipsa
+        omnis vel? Nemo, error tempora. Fugit, voluptate placeat. Corrupti
+        expedita quaerat ullam obcaecati ipsa aut aspernatur fugit facilis eius
+        quasi assumenda quibusdam nulla reprehenderit ea illo explicabo, officia
+        ratione voluptatum atque. Delectus quam consequuntur quaerat alias
+        impedit distinctio magnam voluptas. Molestias nisi aliquam corporis
+        maiores minima sapiente dolores molestiae minus tenetur! Sint adipisci
+        rerum ullam at similique praesentium eos porro voluptatibus incidunt
+        perspiciatis, reprehenderit ut delectus nisi nulla repellendus, aut
+        tempora sit earum vitae velit deserunt possimus reiciendis? Laboriosam
+        vero, incidunt quidem exercitationem dolores rerum dolorum ipsam
+        accusantium atque at, aperiam earum numquam blanditiis. Saepe temporibus
+        possimus, facere repellendus magnam officiis voluptates odit ex commodi
+        distinctio error sint ipsum. Quas sint impedit consectetur facere enim
+        porro eaque officia ex aliquam eos. Distinctio, porro praesentium
+        numquam nostrum, dolore ratione, qui consequuntur amet eligendi ipsa
+        aspernatur officia nam commodi. Temporibus provident voluptatem quam
+        nihil aperiam laboriosam, harum veritatis, soluta recusandae nobis odit
+        at laborum fuga neque accusamus beatae officiis nesciunt? Enim, debitis
+        beatae. Minima dolores libero repellendus? Quod vero quos, ullam
+        recusandae voluptatem quasi error ratione tempore eos culpa ipsum aut
+        quo officiis, sapiente ut earum qui! Asperiores id maiores vitae tenetur
+        similique, cumque dicta nulla ipsa ab adipisci voluptates!
       </div>
     </div>
   );
