f9346fe6833f556cd1ae5a049cf978343ae73bde
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-27 22:53:19 +0300

    chore(component): Remove export default

--- src/app/(main)/_components/tech-stack.tsx
+++ src/app/(main)/_components/tech-stack.tsx
@@ -10,7 +10,10 @@ import { TechStackButtonSkeleton } from "@/components/tech-stack-button/tech-sta
 import { useTheme } from "next-themes";
 import dynamic from "next/dynamic";
 const TechStackButton = dynamic(
-  () => import("@/components/tech-stack-button/tech-stack-button"),
+  () =>
+    import("@/components/tech-stack-button/tech-stack-button").then(
+      (mod) => mod.TechStackButton
+    ),
   {
     ssr: false,
     loading: () => <TechStackButtonSkeleton />,
--- src/components/tech-stack-button/tech-stack-button.tsx
+++ src/components/tech-stack-button/tech-stack-button.tsx
@@ -5,12 +5,10 @@ interface TechStackButtonProps {
   icon: React.ReactElement<CustomSvgProps>;
 }
 
-const TechStackButton = ({ icon }: TechStackButtonProps) => {
+export const TechStackButton = ({ icon }: TechStackButtonProps) => {
   return (
     <div className="p-2 border border-transparent shadow transition-all transform hover:scale-110 hover:bg-primary-foreground hover:border-primary-foreground hover:shadow-lg">
       {React.cloneElement(icon)}
     </div>
   );
 };
-
-export default TechStackButton;
