4cd12f42d04ea78098181d779b3633f8ecddcdba
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-28 12:47:51 +0300

    feat(component): Add title and href

--- src/components/tech-stack-button/tech-stack-button.tsx
+++ src/components/tech-stack-button/tech-stack-button.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { CustomSvgProps } from "../icons/types";
+import Link from "next/link";
 
 interface TechStackButtonProps {
   icon: React.ReactElement<CustomSvgProps>;
+  title: string;
+  href: string;
 }
 
-export const TechStackButton = ({ icon }: TechStackButtonProps) => {
+export const TechStackButton = ({
+  icon,
+  title,
+  href,
+}: TechStackButtonProps) => {
   return (
-    <div className="p-2 border border-transparent shadow transition-all transform hover:scale-110 hover:bg-primary-foreground hover:border-primary-foreground hover:shadow-lg">
-      {React.cloneElement(icon)}
+    <div className="flex flex-col items-center text-center">
+      <Link target="_blank" href={href} rel="noreferrer nofollow">
+        <div className="w-fit p-2 border border-transparent rounded-sm shadow transition-all transform hover:scale-105 hover:border-blue-500 hover:bg-primary-foreground hover:shadow-lg">
+          {React.cloneElement(icon)}
+        </div>
+      </Link>
+      <span className="text-sm font-medium">{title}</span>
     </div>
   );
 };
