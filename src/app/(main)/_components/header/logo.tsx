17f8c84c3f85b9d3440cb3abb9a2b4727f34b191
Emre KÜÇÜKARSLAN <kucukarslanemre@gmail.com>
2024-05-30 12:03:50 +0300

    style(logo): Update logo width and remove gap

--- src/app/(main)/_components/header/logo.tsx
+++ src/app/(main)/_components/header/logo.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <div className="md:flex items-center gap-x-2">
+    <div className="md:flex items-center">
       <Image
         src="/logo-dark.svg"
         height="40"
-        width="40"
+        width="60"
         alt="Logo"
         className="hidden dark:block"
       />
       <Image
         src="/logo.svg"
         height="40"
-        width="40"
+        width="60"
         alt="Logo"
         className="visible dark:hidden"
       />
